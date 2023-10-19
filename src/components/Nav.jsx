4236d3cb3b965b4a89741d475272d9e647ca4b46
Rinamunk <eaarimun@students.eaaa.dk>
2023-10-19 08:14:27 +0200

    navbar closes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,23 +7,36 @@ import * as React from 'react';
 
 
 export default function Nav (){
+    const openNav = () => {
+        const overlay = document.getElementById("overlay");
+        const content = document.querySelector(".content")
+        overlay.style.width = "100%";
+        content.classList.add("content-open");
+    };
+    const closeNav =() => {
+        const overlay = document.getElementById("overlay");
+        const content = document.querySelector(".content");
+        overlay.style.width = "0";
+        content.classList.remove("content-open");
+    }
 
     return(
         <section>
             <input type="checkbox" id="active"></input>
-            <label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
-        <nav class="wrapper">
+            <label htmlFor="active" className="menu-btn"><i className="fas fa-bars"></i></label>
+        <nav className="wrapper">
             <ul>
                 
                 <li>
                     <HomeWorkIcon color="action"/>
-                    <NavLink to="/">Hjem</NavLink></li>
+                    <NavLink to="/" onClick={closeNav}>Hjem</NavLink></li>
               
                 <li>
-                    <ColorLensIcon color="action"/><NavLink to="/kunst">Kunst  </NavLink></li>
+                    <ColorLensIcon color="action"/>
+                    <NavLink to="/kunst" onClick={closeNav}>Kunst  </NavLink></li>
                 <li>
                 <Face2Icon color="action"/>
-                    <NavLink to="/ommig">Om Mig</NavLink></li>
+                    <NavLink to="/ommig" onClick={closeNav}>Om Mig</NavLink></li>
  
             </ul>
             
