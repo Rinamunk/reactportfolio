012491a63dbdb715467277524b25b8e0c83fcce4
Rinamunk <eaarimun@students.eaaa.dk>
2023-10-22 12:18:16 +0200

    new nav function

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,44 +1,48 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import ColorLensIcon from '@mui/icons-material/ColorLens';
-import Face2Icon from '@mui/icons-material/Face2';
-import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+import Face2Icon from "@mui/icons-material/Face2";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
 
-export default function Nav (){
-    const openNav = () => {
-        const overlay = document.getElementById("overlay");
-        const content = document.querySelector(".content")
-        overlay.style.width = "100%";
-        content.classList.add("content-open");
-    };
-    const closeNav =() => {
-        const overlay = document.getElementById("overlay");
-        const content = document.querySelector(".content");
-        overlay.style.width = "0";
-        content.classList.remove("content-open");
-    }
+export default function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return(
-        <section>
-            <input type="checkbox" id="active"></input>
-            <label htmlFor="active" className="menu-btn" onClick={openNav}><i className="fas fa-bars"></i></label>
-        <nav className="wrapper">
-            <ul>
-                
-                <li>
-                    <HomeWorkIcon color="action"/>
-                    <NavLink to="/" onClick={closeNav}>Hjem</NavLink></li>
-              
-                <li>
-                    <ColorLensIcon color="action"/>
-                    <NavLink to="/kunst" onClick={closeNav}>Kunst  </NavLink></li>
-                <li>
-                <Face2Icon color="action"/>
-                    <NavLink to="/ommig" onClick={closeNav}>Om Mig</NavLink></li>
- 
-            </ul>
-            
-        </nav>
-        </section>
+  const openNav = () => {
+    setMenuOpen(true);
+  };
 
-    )
+  const closeNav = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <section>
+      <input type="checkbox" id="active" checked={menuOpen} />
+      <label htmlFor="active" className="menu-btn" onClick={menuOpen ? closeNav : openNav}>
+        <i className="fas fa-bars"></i>
+      </label>
+      <nav className="wrapper">
+        <ul>
+          <li>
+            <HomeWorkIcon color="action" />
+            <NavLink to="/" onClick={closeNav}>
+              Hjem
+            </NavLink>
+          </li>
+          <li>
+            <ColorLensIcon color="action" />
+            <NavLink to="/kunst" onClick={closeNav}>
+              Kunst
+            </NavLink>
+          </li>
+          <li>
+            <Face2Icon color="action" />
+            <NavLink to="/ommig" onClick={closeNav}>
+              Om Mig
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 }
