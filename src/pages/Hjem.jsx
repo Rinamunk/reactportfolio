643026cef74eaa37fc18ce895bafd4fc82fb649c
Rinamunk <eaarimun@students.eaaa.dk>
2023-10-29 21:53:09 +0100

    pushed

--- src/pages/Hjem.jsx
+++ src/pages/Hjem.jsx
@@ -1,13 +1,21 @@
+// Import necessary modules and components
+
 import { CssBaseline, Divider, Stack, ThemeProvider, Typography } from "@mui/material";
 import Actioncard from "../components/projectcard";
 import KompetenceKort from "../components/kompetencer";
 import { useTheme } from "styled-components";
 import { customTheme } from "../themes/themes";
+import Rina from "../assets/rina.jpg"
+import ADHD from "../assets/adhd2.png"
+import Nippon from "../assets/modtaget.gif"
+import Streetfood from "../assets/streetfood.png"
+import Medova from "../assets/logomedova.png"
+import Flex from "../assets/flexlogo.png"
+import Arty from "../assets/front.png"
 
 export default function Hjem() {
          // Hent det aktuelle tema ved hjælp af useTheme hook
      const outerTheme = useTheme();
-     const artPic = "data:../src/assets/artjourney.png";
 
   return (
             <ThemeProvider theme={customTheme(outerTheme)}>
@@ -26,7 +34,8 @@ export default function Hjem() {
             multimediedesign-studerende med en passion for at skabe digitale mesterværker. Lad mig tage dig med på en
             lille rejse gennem, hvem jeg er, og hvad jeg elsker at lave.
           </p>       
-          <img src="../rina.jpg" alt="Rina" className="profile" />
+          <img src={Rina} className="profile"/>
+
 
         </div>
         <KompetenceKort 
@@ -37,37 +46,37 @@ export default function Hjem() {
         <Stack direction="row" spacing={2} display={"flex"} flexWrap={"wrap"}>
 
           <Actioncard
-            image="../src/assets/adhd2.png"
+            image={ADHD}
             title="ADHD"
             subheader="Web dokumentar"
             body="Dette projekt er en webdokumentar, der udforsker Bjørns liv, der først fik diagnosen ADHD som voksen. Fokus er på at levere en spændende oplevelse og give seerne et dybdegående indblik i, hvordan det er at leve med ADHD som voksen. Projektet er en fortælling om udfordringer, triumfer og den unikke rejse, som Bjørn har oplevet gennem livet med ADHD."
           />
             <Actioncard
-            image="../src/assets/modtaget.gif"
+            image={Nippon}
             title="Nippon"
             subheader="UX/UI projekt"
             body="Dette projekt fokuserer på at skabe en brugervenlig og intuitiv brugeroplevelse (UX/UI) for et fiktivt måltidskassefirma ved navn Nippon. Projektet inkorporerer også en smule JavaScript for at forbedre funktionaliteten. Målet er at levere en effektiv digital platform, der gør det let for brugere at bestille måltidskasser fra Nippon."
           />
             <Actioncard
-            image="../src/assets/streetfood.png"
+            image={Streetfood}
             title="Aarhus Streetfood"
             subheader="Mobile First projekt"
             body="Dette projekt har en 'mobile first' tilgang og sigter mod at skabe en visuelt tiltalende opdatering til Aarhus Streetfoods online bestillingsplatform. Hovedfokus er at forbedre brugeroplevelsen på mobile enheder og sikre, at brugerne nemt kan bestille deres yndlingsretter fra Aarhus Streetfood via deres smartphones og tablets."
           />
             <Actioncard
-            image="../src/assets/logomedova.png"
+            image={Medova}
             title="Medova Te"
             subheader="Produkt relanceringsprojekt"
             body="Dette projekt involverer en omfattende relancering af Medova Te, med fokus på nyt emballagedesign og oprettelse af en dedikeret relanceringsside. Projektet repræsenterer mit første eksamensprojekt og er en bestræbelse på at forny og forbedre Medova Te-brandet, herunder dets visuelle identitet og online tilstedeværelse."
           />
            <Actioncard
-            image="../src/assets/flexlogo.png"
+            image={Flex}
             title="FlexVirk"
             subheader="Eksamensprojekt Flexvirk"
             body="Projektet Flexvirk repræsenterer en social økonomisk virksomhed, der arbejder på at skabe jobmuligheder for mennesker på kanten af arbejdsmarkedet. Hjemmesiden, der er udviklet, inkorporerer avanceret JavaScript-funktionalitet for at levere en mere intuitiv brugeroplevelse. Målet er at hjælpe både virksomheden og samfundet ved at tilbyde støtte og muligheder for dem, der har brug for det mest."
           />          
           <Actioncard
-            image="../src/assets/front.png"
+            image={Arty}
             title="ArtJourney"
             subheader="Mit Første Projekt"
             body="Lavet i HTML og CSS for at skabe en visuelt tiltalende oplevelse. ArtJourney er min hyldest til kunst og kreativitet på nettet."
